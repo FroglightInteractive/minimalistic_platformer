028623cd75c6fc8db0b3f263f2133cc164b5a66f
FroglightInteractive <bosbesballon23@gmail.com>
2025-08-05 13:36:18 +0200

    Level 1 starties

--- level_manager.gd
+++ level_manager.gd
@@ -1,1 +1,22 @@
 extends Node
+
+# "level name": preload("level path")
+const LEVELS: Dictionary = {
+	"Level 1": preload("res://Levels/level_1.tscn")
+}
+
+var current_level: int = 1
+
+
+func _ready() -> void:
+	next_level()
+
+
+func next_level() -> void:
+	for child in get_children():
+		child.queue_free()
+	
+	var level_id: String = "Level " + str(current_level)
+	var level = LEVELS[level_id].instantiate()
+	add_child(level)
+	current_level += 1
